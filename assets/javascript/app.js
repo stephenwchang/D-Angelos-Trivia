a12f026b4b23cc3137aa2cd6d22e4d8ef2048bb7
Stephen Chang <stephenwchang@gmail.com>
2019-03-19 11:37:53 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+let triviaQuestions = [
+  question1 = {
+    question: 'Who is the shortest player of all time?',
+    choice1: 'Nate Robinson',
+    choice2: 'Spud Webb',
+    choice3: 'Earl Boykins',
+    answer: 'Muggsy Bogues',
+  },
+
+  question2 = {
+    question: 'Who is the lowest seeded team to ever win an NBA championship?',
+    choice1: '2002-2003 New Jersey Nets',
+    choice2: 'temp',
+    choice3: 'temp',
+    answer: '1994-95 Houston Rockets, sixth seed',
+  },
+
+  question3 = {
+    question: 'What player has the scored the most points in NBA history',
+    choice1: 'temp',
+    choice2: 'temp',
+    choice3: 'temp',
+    answer: 'Kareem Abdul-Jabar',
+  },
+
+  question2 = {
+    question: 'Which team had the best nba record',
+    choice1: 'temp',
+    choice2: 'temp',
+    choice3: 'temp',
+    answer: '2016-2017 Golden State Warriors, 72-10',
+  },
+
+}
